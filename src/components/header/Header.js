e99a592b51f9c939868defbfde3010c5911a07a7
Maximilian Georg <maxi.georg.mg@gmail.com>
2026-01-30 17:03:40 +0100

    fix navigation

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React, {useContext} from "react";
+import {useHistory, useLocation} from "react-router-dom";
 import Headroom from "react-headroom";
 import "./Header.scss";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
@@ -15,6 +16,9 @@ import {
 
 function Header() {
   const {isDark} = useContext(StyleContext);
+  const history = useHistory();
+  const location = useLocation();
+
   const viewExperience = workExperiences.display;
   const viewSkills = skillsSection.display;
   const viewAchievement = achievementSection.display;
@@ -22,10 +26,34 @@ function Header() {
   const viewTalks = talkSection.display;
   const viewResume = resumeSection.display;
 
+  const scrollToSection = hash => {
+    const id = hash.replace("#", "");
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({behavior: "smooth"});
+    }
+  };
+
+  const handleNavClick = (e, hash) => {
+    e.preventDefault();
+
+    // If we're not on the home page, navigate there first
+    if (location.pathname !== "/") {
+      history.push("/");
+      // Wait for the route to change and component to render
+      setTimeout(() => {
+        scrollToSection(hash);
+      }, 100);
+    } else {
+      // If already on home page, just scroll to the section
+      scrollToSection(hash);
+    }
+  };
+
   return (
     <Headroom>
       <header className={isDark ? "dark-menu header" : "header"}>
-        <a href="/" className="logo">
+        <a href="/Max-developerFolio/" className="logo">
           <span className="grey-color"> &lt;</span>
           <span className="logo-name">{greeting.username}</span>
           <span className="grey-color">/&gt;</span>
@@ -41,39 +69,58 @@ function Header() {
         <ul className={isDark ? "dark-menu menu" : "menu"}>
           {viewSkills && (
             <li>
-              <a href="#skills">Skills</a>
+              <a href="#skills" onClick={e => handleNavClick(e, "#skills")}>
+                Skills
+              </a>
             </li>
           )}
           {viewExperience && (
             <li>
-              <a href="#experience">Work Experiences</a>
+              <a
+                href="#experience"
+                onClick={e => handleNavClick(e, "#experience")}
+              >
+                Work Experiences
+              </a>
             </li>
           )}
           {viewAchievement && (
             <li>
-              <a href="#achievements">Achievements</a>
+              <a
+                href="#achievements"
+                onClick={e => handleNavClick(e, "#achievements")}
+              >
+                Achievements
+              </a>
             </li>
           )}
           {viewBlog && (
             <li>
-              <a href="#blogs">Blogs</a>
+              <a href="#blogs" onClick={e => handleNavClick(e, "#blogs")}>
+                Blogs
+              </a>
             </li>
           )}
           {viewTalks && (
             <li>
-              <a href="#talks">Talks</a>
+              <a href="#talks" onClick={e => handleNavClick(e, "#talks")}>
+                Talks
+              </a>
             </li>
           )}
           {viewResume && (
             <li>
-              <a href="#resume">Resume</a>
+              <a href="#resume" onClick={e => handleNavClick(e, "#resume")}>
+                Resume
+              </a>
             </li>
           )}
           <li>
-            <a href="#contact">Contact Me</a>
+            <a href="#contact" onClick={e => handleNavClick(e, "#contact")}>
+              Contact Me
+            </a>
           </li>
           <li>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a>
               <ToggleSwitch />
             </a>
